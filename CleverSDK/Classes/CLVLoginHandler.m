68e656435d9ebd30cd53fb0d31f7a48c941af3f6
Dylan Hurd <dylan.hurd@clever.com>
2017-08-30 15:03:17 -0700

    Use SFAuthenticationController on iOS 11
    If the sdk is running on iOS 11, uses checks for Clever native app and uses
    that if possible, or uses Safari. The README should also be udpated before a new
    release is pushed, as these changes will require small updates to usage
    of the SDK.

--- CleverSDK/Classes/CLVLoginHandler.m
+++ CleverSDK/Classes/CLVLoginHandler.m
@@ -48,37 +48,59 @@
     NSString *state = [CLVOAuthManager generateRandomString:32];
     [CLVOAuthManager setState:state];
 
-    NSString *urlString = [NSString stringWithFormat:@"https://clever.com/oauth/authorize?response_type=code&client_id=%@&redirect_uri=%@&state=%@",
+
+    NSString *safariURLString = [NSString stringWithFormat:@"https://clever.com/oauth/authorize?response_type=code&client_id=%@&redirect_uri=%@&state=%@",
                            [CLVOAuthManager clientId], [CLVOAuthManager redirectUri], [CLVOAuthManager state]];
 
     if (self.districtId) {
-        urlString = [NSString stringWithFormat:@"%@&district_id=%@", urlString, self.districtId];
+        safariURLString = [NSString stringWithFormat:@"%@&district_id=%@", safariURLString, self.districtId];
     }
 
-    // Use SFSVC if iOS version >= 9.0
+    // iOS 8 - always use Safari. Clever App not supported
     if (SYSTEM_VERSION_LESS_THAN(@"9.0")) {
-        [[UIApplication sharedApplication] openURL: [NSURL URLWithString:urlString]];
+        [[UIApplication sharedApplication] openURL: [NSURL URLWithString:safariURLString]];
         return;
     }
 
-    SFSafariViewController *svc = [[SFSafariViewController alloc] initWithURL:[NSURL URLWithString:urlString] entersReaderIfAvailable:NO];
-    if (self.parent.presentedViewController) {
-        [self.parent dismissViewControllerAnimated:YES completion:^{
-            [self.parent presentViewController:svc animated:YES completion:nil];
-        }];
-    } else {
-        [self.parent presentViewController:svc animated:YES completion:nil];
+    // Switch to native Clever app if possible
+    NSURL *cleverAppURL = [NSURL URLWithString:[NSString stringWithFormat:@"com.clever://oauth?client_id=%@&redirect_uri=%@&state=%@",
+                                                [CLVOAuthManager clientId], [CLVOAuthManager redirectUri], [CLVOAuthManager state]]];
+    if ([[UIApplication sharedApplication] canOpenURL:cleverAppURL]) {
+        [[UIApplication sharedApplication] openURL:cleverAppURL];
+        return;
     }
+
+    // Fallbacks:
+    // iOS 9/10 - use SFSafariViewController
+    // iOS 11+ - use Safari
+    if (SYSTEM_VERSION_LESS_THAN(@"11.0")) {
+        SFSafariViewController *svc = [[SFSafariViewController alloc] initWithURL:[NSURL URLWithString:safariURLString] entersReaderIfAvailable:NO];
+        if (self.parent.presentedViewController) {
+            [self.parent dismissViewControllerAnimated:YES completion:^{
+                [self.parent presentViewController:svc animated:YES completion:nil];
+            }];
+        } else {
+            [self.parent presentViewController:svc animated:YES completion:nil];
+        }
+        return;
+    }
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:safariURLString]];
+    return;
 }
 
 - (void)accessTokenReceived:(NSNotification *)notification {
-    if (SYSTEM_VERSION_LESS_THAN(@"9.0")) {
+    // iOS 8 - call success handler
+    // iOS 9/10 - dismiss SFSafariViewController before calling success handler
+    // iOS 11+ - call success handler
+    if SYSTEM_VERSION_LESS_THAN(@"9.0") {
         [CLVOAuthManager callSucessHandler];
-    } else {
+    } else if (SYSTEM_VERSION_LESS_THAN(@"11.0")) {
+        // Must dismiss SFSafariViewController
         [self.parent dismissViewControllerAnimated:NO completion:^{
             [CLVOAuthManager callSucessHandler];
         }];
     }
+    [CLVOAuthManager callSucessHandler];
 }
 
 - (void)oauthAuthorizeFailed:(NSNotification *)notification {
