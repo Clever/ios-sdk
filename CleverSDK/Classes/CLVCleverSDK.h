90558cfce1e223c6dcbf648c89488ad8e915a5a9
Luther Banner III <luther.banner@clever.com>
2018-12-13 16:16:19 -0800

    Bump version

--- CleverSDK/Classes/CLVCleverSDK.h
+++ CleverSDK/Classes/CLVCleverSDK.h
@@ -9,7 +9,7 @@
 #ifndef SimpleLogin_CLVCleverSDK_h
 #define SimpleLogin_CLVCleverSDK_h
 
-#define SDK_VERSION @"iOS-1.0.0"
+#define SDK_VERSION @"iOS-2.0.0"
 
 #import "CLVLoginButton.h"
 #import "CLVOAuthManager.h"
